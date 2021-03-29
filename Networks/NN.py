4d812dfaca178571bc42785caad92f52889ce80d
nlc29 <github0452@rodnam.com>
2021-03-29 14:19:39 +0100

    Comments

--- Networks/NN.py
+++ Networks/NN.py
@@ -19,22 +19,22 @@ class ResidueBlock(nn.Module):
         return out
 
 class FeedForward(nn.Sequential):
-    def __init__(self, dim_model, dim_hidden, dim_out=None):
-        dim_out = dim_model if dim_out is None else dim_out
+    def __init__(self, dim_input, dim_hidden, dim_out=None):
+        dim_out = dim_input if dim_out is None else dim_out
         super().__init__(
-            nn.Linear(dim_model, dim_hidden),
+            nn.Linear(dim_input, dim_hidden),
 	        nn.ReLU(inplace = False),
             nn.Linear(dim_hidden, dim_out)
         ) #initialise nn.Modules
-        # inputs: [n_batch, n_node, dim_model]
-        # outputs: [n_batch, n_node, dim_model]
 
 class Network(nn.Module):
     def __init__(self, inplanes=128, planes=4, kernal=3, padding=1, layers=3, strides_sizes, n_layers, downsample=None):
         self.L_encoder = nn.Sequential(*(ResidueBlock(inplanes, planes, kernal, strides_sizes, padding, layers) for i in range(n_layers)))
-        self.ff = FeedForward(input_dim, hidden_dim, 4)
+        self.ff = FeedForward(16*16, 512, 4)
 
     def forward(self, x):
         inputs = self.L_encoder(x)
+        #also concatenate additional information to the linear layer
+        # additional information: image around head, distance to the nearest block
         inputs = self.ff(inputs)
         return inputs
