c7109db26d5988a8e224d8dc7e8efa635292ea82
nlc29 <github0452@rodnam.com>
2021-03-29 14:22:23 +0100

    Added additional linear layer

--- Networks/NN.py
+++ Networks/NN.py
@@ -30,11 +30,12 @@ class FeedForward(nn.Sequential):
 class Network(nn.Module):
     def __init__(self, inplanes=128, planes=4, kernal=3, padding=1, layers=3, strides_sizes, n_layers, downsample=None):
         self.L_encoder = nn.Sequential(*(ResidueBlock(inplanes, planes, kernal, strides_sizes, padding, layers) for i in range(n_layers)))
-        self.ff = FeedForward(16*16, 512, 4)
+        self.ff = FeedForward(16*16 + 4, 512)
+        self.ff2 = nn.Linear(16*16 +4, 4)
 
     def forward(self, x):
         inputs = self.L_encoder(x)
         #also concatenate additional information to the linear layer
-        # additional information: image around head, distance to the nearest block
+        # additional information: 4 blocks around head, distance to the nearest block
         inputs = self.ff(inputs)
         return inputs
