b2d6b230b8f915936ab7e483aac765a32f4d77db
U-XPS\jiehe <jiehe1998yd@gmail.com>
2021-04-08 16:28:17 +0100

    All obstacles are now of value 1

--- game.py
+++ game.py
@@ -10,8 +10,8 @@ class Game:
     def reset(self):
         self.players = [np.array([s.MAP_SIZE // 4, s.MAP_SIZE // 2]), np.array([s.MAP_SIZE * 3 // 4, s.MAP_SIZE // 2])]
         self.board = np.zeros((s.MAP_SIZE, s.MAP_SIZE))
-        self.board[tuple(self.players[0])] = 1
-        self.board[tuple(self.players[1])] = 2
+        self.board[tuple(self.players[0])] = 2
+        self.board[tuple(self.players[1])] = 3
         self.player_turn = 0
         return self.board, self.players
 
@@ -21,10 +21,10 @@ class Game:
 
     def step(self, action):
         player_pos = self.players[self.player_turn]
-        self.board[tuple(player_pos)] += 2
+        self.board[tuple(player_pos)] = 1
         player_pos += self.actions[action]
         failed = self.out_bounds(player_pos) or self.board[tuple(player_pos)] != 0
         if not failed:
-            self.board[tuple(player_pos)] = 1 + self.player_turn
+            self.board[tuple(player_pos)] = 2 + self.player_turn
         self.player_turn = (self.player_turn + 1) % 2
-        return (self.board, self.players), -10 if failed else 0,failed
+        return (self.board, self.players), -10 if failed else -2 ,failed
