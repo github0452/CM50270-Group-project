5336446c67cdd55d785939b8c103b709b684ecee
maxoverhere <max@hunters.me.uk>
2021-04-08 14:14:00 +0100

    out of bounds

--- game.py
+++ game.py
@@ -15,11 +15,15 @@ class Game:
         self.player_turn = 0
         return self.board, self.players
 
+    def out_bounds(self, cords):
+        bx, by = board.shape()
+        return True if cords[0] > 0 and cords[1] > 0 and cords[0] > bx and cords[1] > by else False
+
     def step(self, action):
         player_pos = self.players[self.player_turn]
         self.board[tuple(player_pos)] += 2
         player_pos += self.actions[action]
-        failed = self.board[tuple(player_pos)] != 0
+        failed = self.out_bounds(player_pos) or self.board[tuple(player_pos)] != 0
         self.board[tuple(player_pos)] = 1 + self.player_turn
         self.player_turn = (self.player_turn + 1) % 2
         return (self.board, self.players), -10 if failed else 0,failed
