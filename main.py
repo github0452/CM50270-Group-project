6d1539d42c7502fbde7b0b96fa2ffab610c6ea23
U-XPS\jiehe <jiehe1998yd@gmail.com>
2021-04-08 14:54:51 +0100

    Two network

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import numpy as np
 from Networks.player import Player
 from game import Game
 import torch
-# from gui import GUI
+from gui import GUI
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 g = Game()
 p = Player(device)
-# window = GUI()
+p2 = Player(device)
+window = GUI()
 
 g.reset()
 
@@ -17,12 +18,16 @@ while True:
     board, players = g.reset()
     failed = False
     rewards = []
+    rewards2 = []
     while not (failed):
         action = p.getAction((board, players[0]))
         (board, players), reward, failed = g.step(action)
         rewards.append(reward)
         if not failed:
-            (board, players), _, failed = g.step(np.random.randint(0, 4))
-        # window.update_frame(board)
+            action = p2.getAction((board, players[1]))
+            (board, players), reward, failed = g.step(action)
+            rewards2.append(reward)
+        window.update_frame(board)
     print(rewards)
     p.backward(np.stack(rewards))
+    p2.backward(np.stack(rewards2))
